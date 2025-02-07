01c22949f17a8ce811f634c88083ad07e6dd0388
tonioquinterorincon <62673855+tonioquinterorincon@users.noreply.github.com>
2025-02-07 08:39:17 -0300

    Add files via upload

---
+++ loadpng.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COVID-19 chest X-ray detection through texture analysis using
+% multi-colinearity diagnosis
+% Antonio Quintero-Rincón code
+% You must put this .m file inside each folder that contains the png format images.
+% input: Chest X-ray images in png format from the COVID-19 Radiography Database
+% Output: Image save each Chest X-ray image, such as COVID-19, Normal, lungOpacity, and viralPneumonia  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all; close all; clc
+
+f=dir('*.png');
+files={f.name};
+for k=1:numel(files)
+  Image{k}=imread(files{k});
+end
+
+%Normal         = Image;
+covid          = Image;
+%lungOpacity    = Image;
+%viralPneumonia = Image;
+
